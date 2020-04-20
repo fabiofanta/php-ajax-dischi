c79bfc9d6009504703b7da4192115211ff04a805
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-04-20 17:42:12 +0200

    3-comm

--- index.php
+++ index.php
@@ -2,11 +2,43 @@
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" href="dist/app.css">
 		<title></title>
 	</head>
 	<body>
+		<header>
+			<div class="header-container wrap">
+				<div class="logo-container">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Logoappliandrospotify.png" alt="Logo">
+				</div>
+			</div>
+		</header>
+		<main>
+			<div class="card-container wrap">
+
+			</div>
+
+		</main>
+		<footer>
+
+		</footer>
 
 	</body>
+
+	<script id="entry-template" type="text/x-handlebars-template">
+		<div class="card">
+			<div class="card-top">
+				<img src="" alt="poster">
+			</div>
+			<div class="card-bottom">
+				<div class="title">
+				</div>
+				<div class="artist"></div>
+				<div class="date"></div>
+			</div>
+		</div>
+	</script>
+
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
