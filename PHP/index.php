73d03a68960f99244d1690f9dbbc4984205b8dac
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-04-21 12:26:33 +0200

    8-rel

--- PHP/index.php
+++ PHP/index.php
@@ -16,31 +16,30 @@
 		</header>
 		<main>
 			<div class="card-container wrap clearfix">
+				<?php
+					include 'data.php';
+				 ?>
 
+				 <?php foreach ($playlists as $key => $playlist) { ?>
+					 <div class="card">
+						 <div class="card-top">
+							 <img src="<?php echo $playlist['Poster'] ?>" alt="poster">
+						 </div>
+						 <div class="card-bottom">
+							 <div class="title"><?php echo $playlist['Album'] ?></div>
+							 <div class="artist small"><?php echo $playlist['Artist'] ?></div>
+							 <div class="date small"><?php echo $playlist['Year'] ?></div>
+						 </div>
+					 </div>
+				<?php }  ?>
 			</div>
 
 		</main>
+
 		<footer>
 
 		</footer>
 
 	</body>
 
-	<script id="card-template" type="text/x-handlebars-template">
-		<div class="card">
-			<div class="card-top">
-				<img src="{{poster}}" alt="poster">
-			</div>
-			<div class="card-bottom">
-				<div class="title"> {{album}}</div>
-				<div class="artist small">{{artist}}</div>
-				<div class="date small">{{year}}</div>
-			</div>
-		</div>
-	</script>
-
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
-	<script src="dist/js/main.js" charset="utf-8"></script>
 </html>
